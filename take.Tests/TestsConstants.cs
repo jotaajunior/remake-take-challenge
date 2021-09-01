8b8e7eccbd8a7f27df5343532d86097e24e988d1
Jota <jjunior@protonmail.ch>
2021-09-01 19:49:51 -0300

    chore: move fake repositories to TestConstants

---
+++ take.Tests/TestsConstants.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+
+using take.Models;
+
+namespace take.Tests
+{
+    public static class TestsConstants
+    {
+        public static List<Repository> FakeRepositories = new List<Repository>
+            {
+                new Repository()
+                {
+                    Name = "1 C#",
+                    Language = "C#",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undeefined"
+                    }
+                },
+
+                new Repository()
+                {
+                    Name = "Should be ignored",
+                    Language = "JavaScript",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undeefined"
+                    }
+                },
+
+                new Repository()
+                {
+                    Name = "2 C#",
+                    Language = "C#",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undefined"
+                    }
+                },
+
+                new Repository()
+                {
+                    Name = "3 C#",
+                    Language = "C#",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undefined"
+                    }
+                },
+
+                new Repository()
+                {
+                    Name = "Should be ignored",
+                    Language = "JavaScript",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undefined"
+                    }
+                },
+
+                new Repository()
+                {
+                    Name = "4 C#",
+                    Language = "C#",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undefined"
+                    }
+                },
+
+                new Repository()
+                {
+                    Name = "C++",
+                    Language = "C++",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undefined"
+                    }
+                },
+
+                new Repository()
+                {
+                    Name = "Not C#",
+                    Language = "Elixir",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undefined"
+                    }
+                },
+
+                new Repository()
+                {
+                    Name = "5 C#",
+                    Language = "C#",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undefined"
+                    }
+                }
+            };
+    }
+}

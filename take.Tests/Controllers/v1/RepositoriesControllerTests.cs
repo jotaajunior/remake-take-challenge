ba038a1b5957caf3403f2030464868e2f8bf880c
Jota <jjunior@protonmail.ch>
2021-09-01 19:49:18 -0300

    test: add test for RepositoriesController

---
+++ take.Tests/Controllers/v1/RepositoriesControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using take.Controllers;
+using take.Facades.Interfaces;
+
+using Shouldly;
+using NSubstitute;
+
+using Xunit;
+
+namespace take.Tests.Controllers.v1
+{
+    public class RepositoriesControllerTests
+    {
+        private readonly RepositoriesController _sut;
+        private readonly IRepositoriesFacade _repositoriesFacade;
+        
+        public RepositoriesControllerTests()
+        {
+            _repositoriesFacade = Substitute.For<IRepositoriesFacade>();
+            _sut = new RepositoriesController(_repositoriesFacade);
+        }
+
+        [Fact]
+        public async Task IndexShouldReturnOkObject()
+        {
+            var result = await _sut.Index();
+
+            result.ShouldBeOfType<OkObjectResult>();
+        }
+    }
+}

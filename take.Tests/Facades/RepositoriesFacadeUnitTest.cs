76bae966749a27d328d2b234045153052d9ac777
Jota <jjunior@protonmail.ch>
2021-09-01 15:18:25 -0300

    test: add extra fields to tests

--- take.Tests/Facades/RepositoriesFacadeUnitTest.cs
+++ take.Tests/Facades/RepositoriesFacadeUnitTest.cs
@@ -23,54 +23,100 @@ namespace take.Tests.Facades
                 new Repository()
                 {
                     Name = "1 C#",
-                    Language = "C#"
+                    Language = "C#",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undeefined"
+                    }
                 },
 
                 new Repository()
                 {
                     Name = "Should be ignored",
-                    Language = "JavaScript"
+                    Language = "JavaScript",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undeefined"
+                    }
                 },
 
                 new Repository()
                 {
                     Name = "2 C#",
-                    Language = "C#"
+                    Language = "C#",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undeefined"
+                    }
                 },
 
                 new Repository()
                 {
                     Name = "3 C#",
-                    Language = "C#"
+                    Language = "C#",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undeefined"
+                    }
                 },
 
                 new Repository()
                 {
-                    Name = "Should be ignored"
+                    Name = "Should be ignored",
+                    Language = "JavaScript",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undeefined"
+                    }
                 },
 
                 new Repository()
                 {
                     Name = "4 C#",
-                    Language = "C#"
+                    Language = "C#",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undeefined"
+                    }
                 },
 
                 new Repository()
                 {
                     Name = "C++",
-                    Language = "C++"
+                    Language = "C++",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undeefined"
+                    }
                 },
 
                 new Repository()
                 {
                     Name = "Not C#",
-                    Language = "Elixir"
+                    Language = "Elixir",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undeefined"
+                    }
                 },
 
                 new Repository()
                 {
                     Name = "5 C#",
-                    Language = "C#"
+                    Language = "C#",
+                    Description = "Description",
+                    Owner = new Owner()
+                    {
+                        Avatar = "undeefined"
+                    }
                 }
             };
 
